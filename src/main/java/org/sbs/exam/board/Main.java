4a0dc76d352d80dab6afd65c52dac2a0d91ee005
SangWon7242 <gotkdqja@gmail.com>
2023-12-17 10:21:56 +0900

    C:/Program Files/Git/usr/article/detail 명령어로 최근 게시물 보기

--- src/main/java/org/sbs/exam/board/Main.java
+++ src/main/java/org/sbs/exam/board/Main.java
@@ -1,12 +1,17 @@
 package org.sbs.exam.board;
 
 import java.util.Scanner;
+/*
+- [o] /usr/article/detail 입력처리
+- [] /usr/article/detail 입력되면 가장 최근 게시물 정보 노출
+*/
 
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
     int articleLastId = 0;
+    Article lastArticle = null;
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 게시판 시작 ==");
@@ -26,10 +31,24 @@ public class Main {
         int id = articleLastId + 1;
 
         Article article = new Article(id, title, body);
+        lastArticle = article;
 
         System.out.println("생성 된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
         articleLastId++;
+      }
+      else if(cmd.equals("/usr/article/detail")) {
+        Article article = lastArticle;
+
+        if (article == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("== 게시물 상세보기 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.body);
       }
       else if(cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
