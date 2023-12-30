63f02ca14a61f25969e63c11d393f1ddddca3dd1
SangWon7242 <gotkdqja@gmail.com>
2023-12-30 09:16:18 +0900

    Util.reverseList를 이용해서 정렬로직을 간소화

--- src/main/java/org/sbs/exam/board/Main.java
+++ src/main/java/org/sbs/exam/board/Main.java
@@ -53,6 +53,8 @@ public class Main {
         System.out.println("-------------------");
         System.out.println("번호 / 제목");
 
+        List<Article> sortedArticles = articles;
+
         boolean orderByIdDesc = true;
 
         if(params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
@@ -60,15 +62,11 @@ public class Main {
         }
 
         if(orderByIdDesc) {
-          for(int i = articles.size() - 1; i >= 0; i--) {
-            Article article = articles.get(i);
-            System.out.printf("%d / %s\n", article.id, article.title);
-          }
+          sortedArticles = Util.reverseList(sortedArticles);
         }
-        else {
-          for (Article article : articles) {
-            System.out.printf("%d / %s\n", article.id, article.title);
-          }
+
+        for (Article article : sortedArticles) {
+          System.out.printf("%d / %s\n", article.id, article.title);
         }
 
         System.out.println("===================");
@@ -187,5 +185,16 @@ class Util {
 
   static String getUrlPathFromUrl(String url) {
     return url.split("\\?", 2)[0];
+  }
+
+  // 이 함수는 원본리스트를 훼손하지 않고, 새 리스트를 만듭니다.
+  // 즉 정렬이 반대인 복사본리스트를 만들어서 반환합니다.
+  public static<T> List<T> reverseList(List<T> list) {
+    List<T> reverse = new ArrayList<>(list.size());
+
+    for ( int i = list.size() - 1; i >= 0; i-- ) {
+      reverse.add(list.get(i));
+    }
+    return reverse;
   }
 }
