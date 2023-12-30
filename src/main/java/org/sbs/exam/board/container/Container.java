13620ca513a4bffb6fb56713e06cf455f3dbdaa4
SangWon7242 <gotkdqja@gmail.com>
2023-12-30 11:46:36 +0900

    UsrArticleController 도입

--- src/main/java/org/sbs/exam/board/container/Container.java
+++ src/main/java/org/sbs/exam/board/container/Container.java
@@ -1,11 +1,15 @@
 package org.sbs.exam.board.container;
 
+import org.sbs.exam.board.article.controller.UsrArticleController;
+
 import java.util.Scanner;
 
 public class Container {
   public static Scanner sc;
+  public static UsrArticleController usrArticleController;
 
   static {
     sc = new Scanner(System.in);
+    usrArticleController = new UsrArticleController();
   }
 }
